074d722688c1d16d3c3139b5ebab90f54c8a4169
Alisha <fogle.alisha@gmail.com>
2023-05-20 09:51:42 -0500

    First Activity finished:  MainActivity.java  activity_main.xml Added: alchemy_128.png enchanting_128.png smithing_128.png skyrim_wallpaper_385.jpg Updated: strings.xml

--- app/src/main/java/com/haman/skyrimcalculator/MainActivity.java
+++ app/src/main/java/com/haman/skyrimcalculator/MainActivity.java
@@ -1,14 +1,33 @@
 package com.haman.skyrimcalculator;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String msg = "";
+        switch (v.getTag().toString()) {
+            case "Smithing":
+                msg = "You pressed the Smithing Button.";
+                break;
+            case "Enchanting":
+                msg = "You pressed the Enchanting Button.";
+                break;
+            case "Alchemy":
+                msg = "You pressed the Alchemy Button.";
+                break;
+        }
+        Log.d("D", msg);
     }
 }
